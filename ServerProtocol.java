8f218f569fda8d8fa051013cb77c5107af8d4061
Nicollet Njora <nicollet.njora05@gmail.com>
2019-05-27 19:30:18 +0300

    Setup server empty class

---
+++ ServerProtocol.java
@@ -0,0 +1,4 @@
+//  This class describes how your server reacts to the messages of your client
+public class ServerProtocol{
+    
+}
---
+++ SocketServer.java
@@ -0,0 +1,4 @@
+// This class  sets up a  socket listening  for your client to  connect  -  and communicates with your client
+public class SocketServer{
+    
+}
